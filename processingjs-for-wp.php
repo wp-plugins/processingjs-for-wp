0447b4b5f7565c98a050db5baef5b700eaa5e057
cageehv <cageehv@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-26 14:55:48 +0000

    NEW: Instructions page added (from plugins page)
    git-svn-id: https://plugins.svn.wordpress.org/processingjs-for-wp/trunk@1146226 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- processingjs-for-wp.php
+++ processingjs-for-wp.php
@@ -3,8 +3,8 @@
 Plugin Name: ProcessingJS for WP
 Plugin URI: http://cagewebdev.com/pjs4wp
 Description: Directly write Processing.js code in your post / page or include an existing .pde sketch
-Version: 1.0
-Date: 04/23/2015
+Version: 1.1
+Date: 04/26/2015
 Author: Rolf van Gelder
 Author URI: http://cagewebdev.com/
 License: GPL2
@@ -28,7 +28,7 @@ License: GPL2
 
 /***********************************************************************************************
  *
- *	LOAD THE SCRIPT(S)
+ *	LOAD THE SCRIPTS
  *
  ***********************************************************************************************/ 
 function pjs4wp_load_scripts()
@@ -37,6 +37,85 @@ function pjs4wp_load_scripts()
 	wp_enqueue_script( 'pjs4wp', plugins_url( '/js/processing.min.js', __FILE__ ), false, '1.4.8' );
 } // pjs4wp_load_scripts()
 add_action( 'init', 'pjs4wp_load_scripts' );
+
+
+/***********************************************************************************************
+ *
+ *	SHOW A LINK TO THE INSTRUCTIONS PAGE ON THE MAIN PLUGINS PAGE
+ *
+ *	Since: v1.1
+ *
+ ***********************************************************************************************/ 
+function pjs4wp_settings_link($links)
+{ 
+  array_unshift($links, '<a href="options-general.php?page=pjs4wp_instructions">'.__('Instructions', 'pjs4wp').'</a>'); 
+  return $links;
+} // pjs4wp_settings_link()
+add_filter('plugin_action_links_'.plugin_basename(__FILE__), 'pjs4wp_settings_link');
+
+
+/***********************************************************************************************
+ *
+ *	ADD THE 'OPTIMIZE DB SETTINGS' ITEM TO THE SETTINGS MENU
+ *
+ *	Since: v1.1
+ *
+ ***********************************************************************************************/ 
+function rvg_pjs4wp_instructions()
+{
+	if (function_exists('add_submenu_page'))
+	{	add_submenu_page(
+			null, // parent_slug: HIDE FROM MENUS!
+			__('ProcessingJS for WP', 'pjs4wp'), // page_title
+			__('ProcessingJS for WP', 'pjs4wp'), // menu_title
+			'manage_options', // capabitily
+			'pjs4wp_instructions', // menu_slug
+			'pjs4wp_instructions' // function
+		);
+	}	
+} // rvg_pjs4wp_admin_menu()
+add_action('admin_menu', 'rvg_pjs4wp_instructions');
+
+
+/***********************************************************************************************
+ *
+ *	SHOW INSTRUCTIONS
+ *
+ *	Since: v1.1
+ *
+ ***********************************************************************************************/ 
+function pjs4wp_instructions()
+{
+?>
+
+<h1 style="margin-bottom: 30px;">ProcessingJS for WP - Instructions</h1>
+<h3 style="margin-bottom: 30px;">With this plugin you can embed a <a href="http://processingjs.org" target="_blank">ProcessingJS</a> sketch in your post or page.</h3>
+<p>You can use the shorttag <strong>[pjs4wp] ... [/pjs4wp]</strong> in two different ways:<br />
+  <br />
+  <strong>1) Put the actual ProcessingJS code directly in a post or page, for instance:</strong><br />
+  <br />
+  [pjs4wp]<br />
+  void setup()<br />
+  {<br />
+  &nbsp;&nbsp;size(200, 200);<br />
+  &nbsp;&nbsp;text("Hello world!", 50, 50);<br />
+  }<br />
+  [/pjs4wp]<br />
+  <br />
+  <strong>2) Use an existing ProcessingJS .pde sketch like this:</strong><br />
+  <br />
+  [pjs4wp url="/wp-content/uploads/my_sketch.js" bordercolor="#000"][/pjs4wp]<br />
+  <br />
+  <strong>IMPORTANT:</strong><br />
+  RENAME YOUR <strong>.pde</strong> FILE TO <strong>.js</strong> BEFORE UPLOADING!<br />
+  So in the example: rename your original <strong>my_sketch.pde</strong> file to <strong>my_sketch.js</strong>!<br />
+  Some servers / browsers are not happy with the .pde extension, that's why...</p>
+<h4>Plugin URL</h4>
+<p><a href="http://cagewebdev.com/processingjs-for-wordpress" rel="nofollow" target="_blank"><strong>http://cagewebdev.com/processingjs-for-wordpress</strong></a></p>
+<h4>Download URL</h4>
+<p><a href="http://wordpress.org/plugins/processingjs-for-wordpress" rel="nofollow" target="_blank"><strong>http://wordpress.org/plugins/processingjs-for-wordpress</strong></a>
+  <?php
+} // pjs4wp_instructions()
 
 
 /***********************************************************************************************
